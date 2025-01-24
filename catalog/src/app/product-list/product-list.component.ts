e16b284d54a26d16b25a344649fe83a76a70ce40
Mireia <mireia@beateam.es>
2025-01-24 12:33:13 +0100

    Agregar estructura inicial del proyecto con componentes y configuración de Angular

---
+++ catalog/src/app/product-list/product-list.component.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.scss'
+})
+export class ProductListComponent {
+
+  public products = [
+    { name: 'Product 1', price: 100 },
+    { name: 'Product 2', price: 200 },
+    { name: 'Product 3', price: 300 },
+  ];
+
+}
